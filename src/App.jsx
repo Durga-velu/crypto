3d0c7c291d42f3926e1a6ff5f44f4e7177d12c00
Durga-velu <durgadevi134babu@gmail.com>
2025-06-06 09:30:24 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,14 @@ const App = () => {
 
   let [currency,setCurrency]=useState()
 
+  console.log(currency);
+
    useEffect(()=>{
    
     const render=async()=>{
+     
+     try{
+
       let data=await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false")
           
       let response=await data.json()
@@ -17,40 +22,45 @@ const App = () => {
       setCurrency(response)
     }
 
+    catch (error){
+        console.log(error);
+        
+    }
+
+
+    }
+
     render()
   },[])
 
-      console.log(currency);
-      
+
   return (
   <>
    <div className='container'>
  
         <table className='table'>
           <tbody>       
-            { currency.map((data)=>{
-                  return <tr id={data.id}>
-                          <td ><div className='coinsImg'>
+           {  currency?  currency.map((data)=>{
+                      return <tr id={data.id}>
+                          <td >
+                            <div className='coinsImg'>
                             <img src={data.image} alt="img"/>
                             </div>
-                                <div className='name'>{data.name}</div></td>
+                           <div className='name'>{data.name}</div>
+                           </td>
                           <td className='symbol'>{data.symbol.toUpperCase()}</td>
                           <td className='curr-Price'>{data.current_price}</td>
                           <td className='volume'>{data.total_volume}</td>
                           <td className="percentage">{parseFloat(data.price_change_24h).toFixed(2)}%</td>
-                          <td className='market-cap'>Mkr cap:{data.market_cap}</td>
+                          <td className='market-cap'>Mkr cap: {data.market_cap}</td>
                         </tr>
-                       })
-              }
-
+                      }):<p className='errorMsg'>API is not loading</p> }
             </tbody>  
         </table>   
-   
    </div>
   </>
-  )
+                  )
 }
-
 export default App
 
 
